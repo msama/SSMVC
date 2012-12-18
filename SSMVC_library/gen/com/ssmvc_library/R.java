6900dcb40fed687fc2adab973e7db40889d4cd89
Mirco Bordoni <mirco.bordoni@gmail.com>
2012-12-18 16:47:27 +0000

    First activity library

--- SSMVC_library/gen/com/ssmvc_library/R.java
+++ SSMVC_library/gen/com/ssmvc_library/R.java
@@ -13,8 +13,21 @@ public final class R {
     public static final class drawable {
         public static int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static int list_view=0x7f070000;
+        public static int menu_settings=0x7f070001;
+    }
+    public static final class layout {
+        public static int activity_list=0x7f030000;
+    }
+    public static final class menu {
+        public static int activity_list=0x7f060000;
+    }
     public static final class string {
-        public static int app_name=0x7f030000;
+        public static int app_name=0x7f040000;
+        public static int hello_world=0x7f040001;
+        public static int menu_settings=0x7f040002;
+        public static int title_activity_list=0x7f040003;
     }
     public static final class style {
         /** 
@@ -38,10 +51,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f040000;
+        public static int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f040001;
+        public static int AppTheme=0x7f050001;
     }
 }

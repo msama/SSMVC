98ac13084e46ea32594ad3504295efc42603fb7d
Mirco Bordoni <mirco.bordoni@gmail.com>
2013-01-07 15:07:46 +0000

    added DS_Store to .gitignore

--- FirstApp/src/com/firstapp/MainActivity.java
+++ FirstApp/src/com/firstapp/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Main Activity - ON CREATE");
     }
 
     @Override
